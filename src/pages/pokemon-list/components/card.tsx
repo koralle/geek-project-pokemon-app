71cfb0b32e6160c9157f1f34e99e6673fa7d9ed2
koralle <33865215+koralle@users.noreply.github.com>
2025-01-29 20:53:57 +0900

    feat: implement list page (#2)
    * feat: implement `TotalContext`, `SetTotalContext`
    
    * feat: wrap content with `TotalContext`, `SetTotalContext`
    
    * feat: implement `PokemonSummaryCard`
    
    * feat: implement `PokemonCollection`
    
    * feat: implement `Pagination`
    
    * feat: implement `Skelton`
    
    ---------
    
    Co-authored-by: koralle <koralle@korarus-MacBook-Air.local>

---
+++ src/pages/pokemon-list/components/card.tsx
@@ -0,0 +1,88 @@
+import { Box, Card, CardBody, Center, Image, Link, Skeleton, Text, VStack } from '@yamada-ui/react'
+import type { PokemonSpeciesId } from '../../../entities'
+import { usePokemonSummary } from '../hooks'
+
+const SkeltonPokemonSummaryCard = () => {
+  return (
+    <>
+      <Card w="full" rounded="2xl">
+        <CardBody>
+          <VStack gap={{ base: 8, lg: 8, md: 8, sm: 6 }}>
+            <Box position="relative">
+              <Center>
+                <Image
+                  src="/monster-ball.svg"
+                  alt="monster-ball"
+                  w={{ base: '160px', lg: '160px', md: '120px', sm: '120px' }}
+                  h={{ base: '160px', lg: '160px', md: '120px', sm: '120px' }}
+                  aria-hidden
+                  position="relative"
+                  decoding="async"
+                />
+                <Text fontSize={{ base: 'lg', md: 'md' }} position="absolute" fontWeight="bold">
+                  Now
+                  <br />
+                  Loading...
+                </Text>
+              </Center>
+            </Box>
+
+            <Skeleton w="full">
+              <Text fontSize={{ base: 'lg', md: 'md' }}>Skelton</Text>
+            </Skeleton>
+          </VStack>
+        </CardBody>
+      </Card>
+    </>
+  )
+}
+
+interface PokemonSummaryCardProps {
+  id: PokemonSpeciesId
+  detailPageUrl: string
+}
+
+const PokemonSummaryCard = ({ id, detailPageUrl }: PokemonSummaryCardProps) => {
+  const { data } = usePokemonSummary(id)
+
+  return (
+    <Card as={Link} href={detailPageUrl} rounded="2xl" position="relative" overflow="hidden">
+      <CardBody>
+        <VStack gap={{ base: 8, lg: 8, md: 8, sm: 6 }}>
+          <Box position="relative">
+            <Center>
+              <Image
+                src="/monster-ball.svg"
+                alt="monster-ball"
+                w={{ base: '160px', lg: '160px', md: '120px', sm: '120px' }}
+                h={{ base: '160px', lg: '160px', md: '120px', sm: '120px' }}
+                aria-hidden
+                position="relative"
+                decoding="async"
+              />
+              {data.imageSrc ? (
+                <Image
+                  position="absolute"
+                  src={data.imageSrc}
+                  alt={data.name}
+                  w={{ base: '160px', lg: '160px', md: '120px', sm: '120px' }}
+                  h={{ base: '160px', lg: '160px', md: '120px', sm: '120px' }}
+                  decoding="async"
+                />
+              ) : (
+                <Text fontSize={{ base: 'lg', md: 'md' }} textAlign="center" fontWeight="bold" position="absolute">
+                  Not Found
+                </Text>
+              )}
+            </Center>
+          </Box>
+          <Text fontSize={{ base: 'lg', md: 'md' }} textAlign="center" fontWeight="bold">
+            {data.name}
+          </Text>
+        </VStack>
+      </CardBody>
+    </Card>
+  )
+}
+
+export { PokemonSummaryCard, SkeltonPokemonSummaryCard }
--- src/pages/pokemon-list/pokemon-list-page.tsx
+++ src/pages/pokemon-list/pokemon-list-page.tsx
@@ -1,3 +1,37 @@
+import { Box, Center, Container, Heading, Text, VStack } from '@yamada-ui/react'
+import { Suspense, useState } from 'react'
+import { PokemonCollection, PokemonCollectionPagination, SkeltonPokemonCollection } from './components'
+import { SetTotalContext, TotalContext } from './lib'
+
 export const PokemonListPage = () => {
-  return <h1>ポケモン一覧ページ</h1>
+  const [total, setTotal] = useState(1000)
+
+  return (
+    <SetTotalContext.Provider value={{ setTotal }}>
+      <TotalContext.Provider value={{ total }}>
+        <Center>
+          <Container maxW={960} h="full">
+            <VStack gap={{ base: 12, lg: 10, md: 8 }}>
+              <Box as="header">
+                <VStack as="hgroup" gap={{ base: 6, lg: 4, md: 4, sm: 4 }}>
+                  <Heading as="h1" w="full" textAlign="center">
+                    ポケモン一覧
+                  </Heading>
+                  <Text w="full" textAlign="center" fontSize={{ base: 'xl', sm: 'md' }}>
+                    好きなポケモンをクリックしてみよう！
+                  </Text>
+                </VStack>
+              </Box>
+              <Suspense fallback={<SkeltonPokemonCollection />}>
+                <PokemonCollection />
+              </Suspense>
+              <Box as="footer">
+                <PokemonCollectionPagination />
+              </Box>
+            </VStack>
+          </Container>
+        </Center>
+      </TotalContext.Provider>
+    </SetTotalContext.Provider>
+  )
 }
