48dfba426c288f126df1830b10df2bf87d6049f0
koralle <koralle@korarus-MacBook-Air.local>
2025-02-08 10:54:03 +0900

    feat: implement BaseLayout

---
+++ src/components/layout.tsx
@@ -0,0 +1,84 @@
+import { MoonIcon, SunIcon } from '@yamada-ui/lucide'
+import { Box, Center, Grid, HStack, IconButton, Image, Spacer, Text, useColorMode } from '@yamada-ui/react'
+import type { ReactNode } from 'react'
+
+const Wrapper = ({ children }: { children: ReactNode }) => {
+  return (
+    <Box maxW={960} minW={320} w="full" px={{ base: 8, md: 4 }} py={6}>
+      {children}
+    </Box>
+  )
+}
+
+const ToggleColorModeButton = () => {
+  const { colorMode, toggleColorMode } = useColorMode()
+
+  return (
+    <IconButton
+      icon={colorMode === 'dark' ? <SunIcon fontSize={6} /> : <MoonIcon fontSize={6} />}
+      variant="primary"
+      fullRounded
+      onClick={toggleColorMode}
+    />
+  )
+}
+
+const Header = () => {
+  const { colorMode } = useColorMode()
+
+  return (
+    <Center
+      as="header"
+      backdropBlur="0.5rem"
+      backdropFilter="auto"
+      backdropSaturate="180%"
+      position="sticky"
+      top={0}
+      left={0}
+      right={0}
+      zIndex={1}
+    >
+      <Wrapper>
+        <HStack>
+          {colorMode === 'light' ? (
+            <Image src="title.png" alt="ポケモンずかん" decoding="auto" />
+          ) : (
+            <Image src="title-dark.png" alt="ポケモンずかん" decoding="auto" />
+          )}
+          <Spacer />
+          <ToggleColorModeButton />
+        </HStack>
+      </Wrapper>
+    </Center>
+  )
+}
+
+const Main = ({ children }: { children: ReactNode }) => {
+  return (
+    <Center as="main" id="main" h="200svh">
+      <Wrapper>{children}</Wrapper>
+    </Center>
+  )
+}
+
+const Footer = () => {
+  return (
+    <Center as="footer">
+      <Wrapper>
+        <Center>
+          <Text>&copy; 2025 koralle</Text>
+        </Center>
+      </Wrapper>
+    </Center>
+  )
+}
+
+export const Layout = ({ children }: { children: ReactNode }) => {
+  return (
+    <Grid minW="100svi" minH="100svb" templateRows="max-content 1fr max-content" gap={0}>
+      <Header />
+      <Main>{children}</Main>
+      <Footer />
+    </Grid>
+  )
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,21 @@
 import { QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { UIProvider } from '@yamada-ui/react'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { App } from './app'
+import { Layout } from './components/layout'
 import { queryClient } from './lib/query-client'
 
 // biome-ignore lint/style/noNonNullAssertion:
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <UIProvider>
+        <Layout>
+          <App />
+        </Layout>
+      </UIProvider>
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   </StrictMode>,
