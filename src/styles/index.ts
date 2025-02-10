b27041c70ad1a3b4f3f36ad1255ff7ad5f6f7608
koralle <koralle@korarus-MacBook-Air.local>
2025-02-10 23:55:46 +0900

    feat(yamada-ui): add tokens of brand-colors

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -1,9 +1,11 @@
 import { extendTheme } from '@yamada-ui/react'
 import type { UsageTheme } from '@yamada-ui/react'
 import { breakpoints } from './breakpoints'
+import { tokens } from './tokens'
 
 const customTheme: UsageTheme = {
   breakpoints,
+  ...tokens,
 }
 
 export const theme = extendTheme(customTheme)({ omit: ['breakpoints'] })
---
+++ src/styles/tokens/colors.ts
@@ -0,0 +1,7 @@
+import { generate } from '@yamada-ui/react'
+import type { ThemeTokens } from '@yamada-ui/react'
+
+export const colors: ThemeTokens = {
+  pokedex: generate.tones('#b94646'),
+  pokedexDark: generate.tones('#c56767'),
+}
