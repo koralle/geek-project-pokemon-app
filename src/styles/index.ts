eaa3c73d5dd7107c777b8fa4940cc6226b889889
koralle <koralle@korarus-MacBook-Air.local>
2025-02-10 23:53:51 +0900

    feat(yamada-ui): add custom breakpoints

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -1,6 +1,9 @@
 import { extendTheme } from '@yamada-ui/react'
 import type { UsageTheme } from '@yamada-ui/react'
+import { breakpoints } from './breakpoints'
 
-const customTheme: UsageTheme = {}
+const customTheme: UsageTheme = {
+  breakpoints,
+}
 
-export const theme = extendTheme(customTheme)()
+export const theme = extendTheme(customTheme)({ omit: ['breakpoints'] })
