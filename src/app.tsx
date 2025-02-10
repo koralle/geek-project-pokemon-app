3aee9b58451d5ce76856a81e91dae35605a4f5f9
koralle <koralle@korarus-MacBook-Air.local>
2025-02-10 22:59:13 +0900

    feat(yamada-ui): implement `TotalContext`, `SetTotalContext`

--- src/app.tsx
+++ src/app.tsx
@@ -1,13 +1,19 @@
 import { useState } from 'react'
 import type { ReactNode } from 'react'
 import { PageContext, SetPageContext } from './lib/contexts/page-context'
+import { SetTotalContext, TotalContext } from './lib/contexts/total-context'
 
 const Providers = ({ children }: { children: ReactNode }) => {
   const [page, setPage] = useState(1)
+  const [total, setTotal] = useState(1000)
 
   return (
     <SetPageContext.Provider value={{ setPage }}>
-      <PageContext.Provider value={{ page }}>{children}</PageContext.Provider>
+      <SetTotalContext.Provider value={{ setTotal }}>
+        <TotalContext.Provider value={{ total }}>
+          <PageContext.Provider value={{ page }}>{children}</PageContext.Provider>
+        </TotalContext.Provider>
+      </SetTotalContext.Provider>
     </SetPageContext.Provider>
   )
 }
