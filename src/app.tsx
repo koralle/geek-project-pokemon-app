83c4628cb1a4690bef5d3338c55bb571e34eb566
koralle <koralle@korarus-MacBook-Air.local>
2025-02-10 22:57:00 +0900

    feat(yamada-ui): implement `PageContext`, `SetPageContext`

--- src/app.tsx
+++ src/app.tsx
@@ -1,3 +1,21 @@
+import { useState } from 'react'
+import type { ReactNode } from 'react'
+import { PageContext, SetPageContext } from './lib/contexts/page-context'
+
+const Providers = ({ children }: { children: ReactNode }) => {
+  const [page, setPage] = useState(1)
+
+  return (
+    <SetPageContext.Provider value={{ setPage }}>
+      <PageContext.Provider value={{ page }}>{children}</PageContext.Provider>
+    </SetPageContext.Provider>
+  )
+}
+
 export const App = () => {
-  return <h1>Hello, world!</h1>
+  return (
+    <Providers>
+      <h1>Hello, world!</h1>
+    </Providers>
+  )
 }
