7736126b5ed49c2f5c5a3f44d6e0e74c99834ddc
koralle <koralle@korarus-MacBook-Air.local>
2025-02-10 23:57:47 +0900

    feat(yamada-ui): add semantic-colors

---
+++ src/styles/semantics.ts
@@ -0,0 +1,11 @@
+import type { ThemeSemantics } from '@yamada-ui/react'
+
+export const semantics: ThemeSemantics = {
+  colors: {
+    black: ['#262626', '#1a1a1a'],
+    white: ['#fcfcfc', '#e0e0e0'],
+  },
+  colorSchemes: {
+    primary: ['pokedex', 'pokedexDark'],
+  },
+}
