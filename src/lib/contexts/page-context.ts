ac650f8fd6d112312e2bd60e1166f368156def36
koralle <koralle@korarus-MacBook-Air.local>
2025-02-10 22:57:00 +0900

    feat(yamada-ui): implement `PageContext`, `SetPageContext`

---
+++ src/lib/contexts/page-context.ts
@@ -0,0 +1,49 @@
+import { createContext, useContext } from 'react'
+import type { Dispatch, SetStateAction } from 'react'
+
+export interface PageContextState {
+  page: number
+}
+
+export interface SetPageContextState {
+  setPage: Dispatch<SetStateAction<number>>
+}
+
+const createPageContext = () => {
+  const PageContext = createContext<PageContextState | undefined>(undefined)
+  PageContext.displayName = 'PageContext'
+
+  const usePageContext = () => {
+    const pageContext = useContext(PageContext)
+    if (!pageContext) {
+      const error = new Error('usePageContext must be used within a PageContext')
+      error.name = 'PageContextError'
+      throw error
+    }
+
+    return pageContext
+  }
+
+  return { PageContext, usePageContext }
+}
+
+const createSetPageContext = () => {
+  const SetPageContext = createContext<SetPageContextState | undefined>(undefined)
+  SetPageContext.displayName = 'SetPageContext'
+
+  const useSetPageContext = () => {
+    const setPageContext = useContext(SetPageContext)
+    if (!setPageContext) {
+      const error = new Error('useSetPageContext must be used within a SetPageContext')
+      error.name = 'SetPageContextError'
+      throw error
+    }
+
+    return setPageContext
+  }
+
+  return { SetPageContext, useSetPageContext }
+}
+
+export const { PageContext, usePageContext } = createPageContext()
+export const { SetPageContext, useSetPageContext } = createSetPageContext()
