869ab80d410b123161dab7eac2014bc6754e2a40
koralle <koralle@korarus-MacBook-Air.local>
2025-02-10 22:45:53 +0900

    feat(yamada-ui): setup Yamada UI

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,19 @@
 import { QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { UIProvider } from '@yamada-ui/react'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { App } from './app'
 import { queryClient } from './lib/query-client'
+import { customConfig } from './styles/config'
 
 // biome-ignore lint/style/noNonNullAssertion:
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <UIProvider config={customConfig}>
+        <App />
+      </UIProvider>
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   </StrictMode>,
